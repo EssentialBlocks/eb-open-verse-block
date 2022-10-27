6f8b469e068bbb58ac6e53be341bb178b9b10d39
Rahat Hossain <rahat.feni99@gmail.com>
2022-10-27 15:48:54 +0600

    removed include

--- includes/class-openverse-ajax.php
+++ includes/class-openverse-ajax.php
@@ -262,44 +262,22 @@ class EB_Openverse_Block_Ajax {
      */
     private static function do_upload( $url, $title = null ) {
 
-        // if (file_exists( ABSPATH . '/wp-load.php' )){  
-        //     require_once ABSPATH . '/wp-load.php';
-        // }
-
-        if (!function_exists('wp_crop_image')){  
-            require_once(ABSPATH . "wp-admin" . '/includes/image.php');
-        }
-        
-        if (!function_exists('get_file_description')){
-            require_once(ABSPATH . "wp-admin" . '/includes/file.php');
-        }
-        
-        if (!function_exists('media_upload_tabs')){
-            require_once(ABSPATH . "wp-admin" . '/includes/media.php');
-        }
-
-
-        // require_once ABSPATH . '/wp-admin/includes/image.php';
-        // require_once ABSPATH . '/wp-admin/includes/file.php';
-        // require_once ABSPATH . '/wp-admin/includes/media.php';
-
+		if(!function_exists('download_url') && !function_exists('media_handle_sideload')){
+			return false;
+		}
         // Download url to a temp file
         $tmp = download_url( $url );
         if ( is_wp_error( $tmp ) ) {
 			return false;
         }
 
-        // Get the filename and extension ("photo.png" => "photo", "png")
         $filename  = pathinfo( $url, PATHINFO_FILENAME );
         $extension = pathinfo( $url, PATHINFO_EXTENSION );
 
-        // An extension is required or else WordPress will reject the upload
         if ( ! $extension ) {
-            // Look up mime type, example: "/photo.png" -> "image/png"
             $mime = mime_content_type( $tmp );
             $mime = is_string( $mime ) ? sanitize_mime_type( $mime ) : false;
 
-            // Only allow certain mime types because mime types do not always end in a valid extension (see the .doc example below)
             $mime_extensions = array(
                 // mime_type         => extension (no period)
                 'image/jpg'  => 'jpg',
@@ -309,16 +287,12 @@ class EB_Openverse_Block_Ajax {
             );
 
             if ( isset( $mime_extensions[ $mime ] ) ) {
-                // Use the mapped extension
                 $extension = $mime_extensions[ $mime ];
             } else {
-                // Could not identify extension
                 @unlink( $tmp );
                 return false;
             }
         }
-
-        // Upload by "sideloading": "the same way as an uploaded file is handled by media_handle_upload"
         $args = array(
             'name'     => "$filename.$extension",
             'tmp_name' => $tmp,
@@ -327,7 +301,6 @@ class EB_Openverse_Block_Ajax {
         // Do the upload
         $attachment_id = media_handle_sideload( $args, 0, $title );
 
-        // Cleanup temp file
         @unlink( $tmp );
 
         // Error uploading
@@ -335,7 +308,6 @@ class EB_Openverse_Block_Ajax {
 			return false;
         }
 
-        // Success, return attachment ID (int)
         return (int) $attachment_id;
     }
 
