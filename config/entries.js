ae5774443ae08da960474363718218c6a9e938b8
Sumaiya Siddika <to.sumaiyasiddika@gmail.com>
2022-11-22 14:55:08 +0600

    clean helper

--- config/entries.js
+++ config/entries.js
@@ -7,29 +7,23 @@ export { default as BorderShadowControl } from "../controls/src/controls/border-
 export { default as ResponsiveDimensionsControl } from "../controls/src/controls/dimensions-control-v2";
 export { default as ResponsiveRangeController } from "../controls/src/controls/responsive-range-control";
 export { default as TypographyDropdown } from "../controls/src/controls/typography-control-v2";
-export { default as faIcons } from "../controls/src/extras/faIcons";
 
 import "../controls/src/group-controls";
 export { default as AdvancedControls } from "../controls/src/group-controls/components/advanced-controls";
 
 //Export Helper Functions
 export {
-  mimmikCssForResBtns,
-  mimmikCssOnPreviewBtnClickWhileBlockSelected,
   softMinifyCssStrings,
   generateBackgroundControlStyles,
   generateDimensionsControlStyles,
   generateTypographyStyles,
   generateBorderShadowStyles,
   generateResponsiveRangeStyles,
-  mimmikCssForPreviewBtnClick,
   duplicateBlockIdFix,
   generateDimensionsAttributes,
   generateTypographyAttributes,
   generateBackgroundAttributes,
   generateBorderShadowAttributes,
   generateResponsiveRangeAttributes,
-  textInsideForEdit,
-  getFlipTransform,
   ebConditionalRegisterBlockType,
 } from "../controls/src/helpers";
--- dist/index.asset.php
+++ dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e46358563b689e9e0324');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'a6d4205784943ac82782');
