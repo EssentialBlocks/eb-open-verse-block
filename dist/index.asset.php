82961bc2e79c137301a14faee028133e4031d998
Sumaiya Siddika <to.sumaiyasiddika@gmail.com>
2024-05-14 15:42:10 +0600

    build

--- dist/index.asset.php
+++ dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '9148e6880f624462b8e7');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c8db39c03c242469ee90');
